62a101d863905a473c9e28e0e3c56ece38b1e682
jeromeds <hello@jeromeds.com>
2016-04-04 22:54:15 -0400

    Clean up. Remove what is not used for now.

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,23 @@
-import config from './config';
 import Keyboard from './keyboard';
 import Mouse from './mouse';
 import Recorder from './recorder';
-import Scroll from './scroll';
-import Timer from './timer';
-import moment from 'moment';
-import Data from '../db/eventsLog';
 
 const Mimic = (params) => {
-
   const container = document.getElementById(params.container),
         recorder = Recorder('session'),
         keyboard = Keyboard(),
-        mouse = Mouse(container),
-        scroll = Scroll();
+        mouse = Mouse(container);
 
   const startRecording = () => {
-    console.log(Data);
     recorder.record('Start recording');
     keyboard.record();
     mouse.record();
-    scroll.record();
   }
 
   const stopRecording = () => {
     keyboard.stop();
     mouse.stop();
-    scroll.stop();
     recorder.record('Stop recording');
-    console.log(Data);
   }
 
   const init = () => {
